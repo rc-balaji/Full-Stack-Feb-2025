5babf5976c63254d0243334e947d1a2a927c1253
rc-balaji <rcbalaji2003@gmail.com>
2025-02-19 12:21:39 +0530

    Day-3

---
+++ Day-3/inClass/script.js
@@ -0,0 +1,40 @@
+arr = [
+  {
+    fname: "avi",
+    lname: "nash",
+    roll_no: 2,
+    address: "tiruppur",
+  },
+  {
+    fname: "raj",
+    lname: "kumar",
+    roll_no: 25,
+    address: "chennai",
+  },
+  {
+    fname: "ravi",
+    lname: "kumar",
+    roll_no: 5,
+    address: "covai",
+  },
+];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log("Full name:", arr[i].fname, arr[i].lname);
+//   console.log("Roll_no:", arr[i].roll_no);
+//   console.log("address:", arr[i].address);
+// }
+
+// arr.forEach((item, index) => {
+//   console.log("Full name:", item.fname, item.lname);
+//   console.log("Roll_no:", item.roll_no);
+//   console.log("address:", item.address);
+
+//   console.log(index);
+// });
+
+new_arr = ["sasa", "asas", "asasadf", "ass"];
+
+new_arr.forEach((num, index) => {
+  console.log(num, index);
+});
